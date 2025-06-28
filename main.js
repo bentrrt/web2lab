d1662ec2431b58a1181a00525a178602b64ae9a6
bentrrt <giabao100705@gmail.com>
2025-06-28 13:11:21 +0700

    video 3 & coding challenge

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image:'./assets/images/socks_green.jpg',
+            // coding challenge - GCS230307
+            url:'https://amazon.com',
         }
     }
 })
