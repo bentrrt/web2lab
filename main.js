521147c1256da25269d02d01f045b6d9e483abf8
bentrrt <giabao100705@gmail.com>
2025-07-07 21:58:37 +0700

    completed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            product: 'Socks/GCS230307',
             image:'./assets/images/socks_green.jpg',
             // coding challenge - GCS230307
             url:'https://amazon.com',
